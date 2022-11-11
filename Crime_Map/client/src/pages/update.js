08a2cf4429c66515f8449dcd122c076fd1908ab3
Elijah-Ye <elijah.gaohan.ye@gmail.com>
2022-11-10 23:55:23 -0600

    update draft 1

--- Crime_Map/client/src/pages/update.js
+++ Crime_Map/client/src/pages/update.js
@@ -1,13 +1,43 @@
-import React from "react";
-  
-const Update = () => {
+import './update.css';
+import { useState } from 'react';
+import Axios from 'axios';
+import { id } from 'date-fns/locale';
+
+function Update() {
+  const [newDescription, setNewDescription] = useState("");
+  const [newId, setNewId] = useState(0);
+
+  const updateCrime = () => {
+    Axios.put("http://localhost:3001/update",{
+      description: newDescription,
+      id: newId
+    }).then((response) => {
+      alert("update")
+    })
+  }
+
   return (
-    <div>
-      <h1>
-        UPDATE SQL
-      </h1>
+    <div className="update">
+      <p> Insert the CrimeID you want to update </p>
+      <input 
+          type="number" 
+          placeholder="00000" 
+          onChange = {(event) => {
+            setNewId(event.target.value);
+          }}
+      />
+      <p> Write the new description for this crime </p>
+      <input 
+          type="text" 
+          placeholder="This is a new description" 
+          onChange = {(event) => {
+            setNewDescription(event.target.value);
+          }}
+      />
+      <button onClick={updateCrime}> Update </button>
     </div>
   );
-};
-  
+}
+
 export default Update;
+
